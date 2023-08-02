f09bcb79a08912a63a4cafac9ea749bed67d6e0d
kimhyeok <kimmang940715@naver.com>
2023-08-03 01:24:20 +0900

    add create Photon Lobby

--- photonRnD/Assets/Launcher.cs
+++ photonRnD/Assets/Launcher.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
- public class Launcher : MonoBehaviour
+using Photon.Realtime;
+ public class Launcher : MonoBehaviourPunCallbacks
     {
+        
         #region Private Serializable Fields
-
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 4;
         #endregion
+        
         #region Private Fields
         string gameVersion = "1";
 
         #endregion
 
+        #region MonoBehaviourPunCallbacks Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinRandomRoom();
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        #endregion
+        
         #region MonoBehaviour CallBacks
         void Awake()
         {
@@ -37,6 +57,19 @@ using Photon.Pun;
                 PhotonNetwork.ConnectUsingSettings();
                 PhotonNetwork.GameVersion = gameVersion;
             }
+        }
+        
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
         }
         #endregion
 
